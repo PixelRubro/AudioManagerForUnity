5437ec6e32a616e7e7d5c3b03ba9f57f0db43797
msavioti <angelosavioti@gmail.com>
2021-12-26 15:04:40 -0300

    created a sound settings menu script

---
+++ AudioSettingsMenu.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YoukaiFox.AudioManager
+{
+    public class AudioSettingsMenu : MonoBehaviour
+    {
+        #region Actions
+        #endregion
+
+        #region Serialized fields
+
+        [SerializeField]
+        private bool _updateOnValueChanged = true;
+
+        [SerializeField]
+        private Toggle _muteSoundToggle;
+
+        [SerializeField]
+        private Slider _musicVolumeSlider;
+
+        [SerializeField]
+        private Slider _sfxVolumeSlider;
+
+        [SerializeField]
+        [Tooltip("An audio clip to play when adjusting the volume.")]
+        private AudioClip _audioTextClip;
+
+        #endregion
+
+        #region Non-serialized fields
+
+        private AudioManager _audioManager;
+
+        #endregion
+
+        #region Constant fields
+        #endregion
+
+        #region Unity events
+
+        private void Start()
+        {
+            _audioManager = FindObjectOfType<AudioManager>();
+            SetUpListeners();
+        }
+
+        private void OnEnable()
+        {
+            LoadSettings();
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void UpdateSoundSettings()
+        {
+            if (AudioManager.Instance == null)
+            {
+                return;
+            }
+
+            var settings =  new AudioManager.AudioSettings();
+            settings.IsMuted = _muteSoundToggle.isOn;
+            settings.BgmVolume = _musicVolumeSlider.value;
+            settings.SfxVolume = _sfxVolumeSlider.value;
+            AudioManager.Instance.UpdateSettings(settings);
+        }
+
+
+        #endregion
+
+        #region Protected methods
+        #endregion
+
+        #region Private methods
+
+        private void LoadSettings()
+        {
+            if (AudioManager.Instance == null)
+            {
+                return;
+            }
+
+            var settings = AudioManager.Instance.GetCurrentSettings();
+            _muteSoundToggle.isOn = settings.IsMuted;
+            _musicVolumeSlider.value = settings.BgmVolume;
+            _sfxVolumeSlider.value = settings.SfxVolume;
+        }
+
+        private void SetUpListeners()
+        {
+            if (_updateOnValueChanged)
+            {
+                _muteSoundToggle.onValueChanged.AddListener(delegate {UpdateSoundSettings();});
+                _musicVolumeSlider.onValueChanged.AddListener(delegate {UpdateSoundSettings();});
+                _sfxVolumeSlider.onValueChanged.AddListener(delegate {UpdateSoundSettings();});
+            }
+        }
+
+        #endregion 
+    }
+}
